3fcdb1d9016109433e7518cbeb380a1b9e6897b7
Subhan Saadat Khan <subhansaadat@gmail.com>
2023-07-09 01:40:32 +0500

    Add css for responsiveness (smaller screen)

--- index.js
+++ index.js
@@ -15,3 +15,14 @@ function opentab(tabName, e){
     e.currentTarget.classList.add("active-link");
     document.getElementById(tabName).classList.add("active-tab")
 }
+
+// responsiveness (smaller screen)
+var sideMenu = document.getElementsByClassName("sidemenu");
+
+function openMenu(){
+    sideMenu[0].style.right = "0";
+}
+
+function closeMenu(){
+    sideMenu[0].style.right = "-200px";
+}
